ef2d2ae6a57020a6e1a62a596043490f6bbe43c3
mechaphysis <triptofante@gmail.com>
2018-07-23 18:44:15 +0200

    feat: Add third suite and its test for feed content

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -93,16 +93,39 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+    describe('Initial Entries', function() {
+      /* This is our third test suite that will test the Feed load:
+      */
+      /* This test ensures  that when the loadFeed asynchronous function
+      * function is called and completes its work, there is at least
+      * a single .entry element within the .feed container.
+      *
+      * For replicating loadFeed() asynchronous behaviour we use beforeEach()
+      * and done() jasmine functions. So the loadFeed will be called and completed
+      * before passing the first test of the suite:
+      */
+      let firstEntry;
+      beforeEach(function(done) {
+        loadFeed(0,function() {
+          firstEntry = $('.feed .entry');
+          done();
+        });
+      });
+      // we check that the .feed as at least a .entry element:
+      it('should have at least an .entry element', function(done) {
+        expect(firstEntry.length).toBeDefined();
+        /* we use again the done() function to tell this part of the test that
+         * its making use of the loadFeed() asynchronous functionality.
          */
+        done();
+      });
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+
+    })
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
