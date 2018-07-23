14dc1f025c3ad74a6b37d77fae40b9dc7a4ffefe
mechaphysis <triptofante@gmail.com>
2018-07-23 17:39:15 +0200

    feat: Write tests for URLs and names in allFeeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,26 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        /* For doing this we wrap the expects of jasmine inside
+        *  a for...of loop that goes through every feed in allFeeds:
+        */
+        it('URL are defined', function() {
+          for (let feed of allFeeds) {
+            expect(feed.url).toBeDefined();
+            expect(feed.url.length).not.toBe(0);
+          };
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('names are defined', function() {
+          for(let feed of allFeeds) {
+            expect(feed.name).toBeDefined();
+            expect(feed.name.length).not.toBe(0);
+          };
+        });
     });
 
 
